cd0bee541ccd94d9a52c13c5923c770e6d43621e
Orlando Timmerman <rt582@cam.ac.uk>
2024-11-06 12:08:37 +0000

    updated regridding

--- cmipper/extract_levels.py
+++ cmipper/extract_levels.py
@@ -1,61 +1,7 @@
-# from concurrent.futures import ProcessPoolExecutor, as_completed
-# from cmipper import config, processing, file_ops
-# from pathlib import Path
-# from tqdm import tqdm
-
-# RAW_DATA_DIR = config.ESGPULL_DATA_DIR
-# NUM_CORES = 16  # Specify the number of cores to use
-# data_processing_config = file_ops.read_yaml(config.TMP_DIR / "data_processing.yaml")
-
-# # set up download parameters
-# # select_level = data_processing_config["select_level"]
-# DO_DELETE_ORIGINAL = data_processing_config["do_delete_original"]
-
-# def extract_levels(subdir): # TODO: more options
-#     """Process a single subdirectory."""
-#     print(subdir)
-#     processing.process_raw_data_directory(subdir, chunk_schema=None, delete_og=DO_DELETE_ORIGINAL)
-
-# def main():
-#     # Fetch all subdirectories, ignoring those with 'deprecated'
-#     subdirs = [subdir for subdir in Path(RAW_DATA_DIR).glob("**/")
-#                     if "deprecated" not in str(subdir) and subdir.is_dir()]
-
-#     # Use ProcessPoolExecutor to parallelize the processing of subdirectories
-#     with ProcessPoolExecutor(max_workers=NUM_CORES) as executor:
-#         futures = {executor.submit(extract_levels, subdir): subdir for subdir in subdirs}
-#         for future in tqdm(as_completed(futures), total=len(futures),
-#               desc="Processing raw data to extract levels..."):
-#             subdir = futures[future]
-#             try:
-#                 future.result()  # Get the result to raise any exceptions
-#             except Exception as e:
-#                 print(f"Error processing {subdir}: {e}")
-
-
-# def extract_levels(subdir):  # TODO: more options
-#     """Process a single subdirectory."""
-#     print(subdir)
-#     processing.process_raw_data_directory(
-#         subdir, chunk_schema=None, delete_og=DO_DELETE_ORIGINAL
-#     )
-
-
-# def main():
-#     # Fetch all subdirectories, ignoring those with 'deprecated'
-#     subdirs = [
-#         subdir
-#         for subdir in Path(RAW_DATA_DIR).glob("**/")
-#         if "deprecated" not in str(subdir) and subdir.is_dir()
-#     ]
-
-#     # Process each subdirectory sequentially
-#     for subdir in tqdm(subdirs, desc="Processing raw data to extract levels..."):
-#         extract_levels(subdir)
-
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from pathlib import Path
 from tqdm import tqdm
+import xarray as xa
 from cmipper import utils, config, processing, file_ops
 
 RAW_DATA_DIR = config.ESGPULL_DATA_DIR
@@ -67,8 +13,8 @@ SELECT_LEVEL = data_processing_config["select_level"]
 DO_REGRID = data_processing_config["do_regrid"]
 REMAP_METHOD = data_processing_config["remap_method"]
 OUTPUT_GRID = data_processing_config["output_grid"]
-
-
+SELECT_LEVEL = False
+DO_REGRID = True
 # regrid dir
 regridded_data_dir_fp = config.TEST_DATA_DIR
 
@@ -130,53 +76,55 @@ def process_raw_data_directory(raw_data_dir_fp: Path, chunk_schema: dict = {"tim
                 if DO_DELETE_ORIGINAL:
                     nc_file.unlink()
 
-        # regridding
-        if DO_REGRID:
-            # select directories either "extracted_lev" which are subdirectories of raw_data_dir_fp,
-            # or which are "tos", "rsds"
-            if "extracted_lev" in str(raw_data_dir_fp) or raw_data_dir_fp.name in [
-                "tos",
-                "rsds",
-            ]:
-                nc_files = list(raw_data_dir_fp.glob("*.nc"))
-                remap_template_fp = processing.handle_cdo_template(
-                    nc_files[0],
-                    raw_data_dir_fp,
-                    OUTPUT_GRID,
+    # regridding
+    if DO_REGRID:
+        # select directories either "extracted_lev" which are subdirectories of raw_data_dir_fp,
+        # or which are "tos", "rsds"
+
+        if any(
+            keyword in str(raw_data_dir_fp)
+            for keyword in ["extracted_lev", "tos", "rsds"]
+        ):
+
+            nc_files = list(raw_data_dir_fp.glob("*.nc"))
+            remap_template_fp = processing.handle_cdo_template(
+                xa.open_dataset(nc_files[0]),
+                raw_data_dir_fp,
+                OUTPUT_GRID,
+            )
+            # make new directory for regridded data
+            regridded_dir = (
+                regridded_data_dir_fp
+                / raw_data_dir_fp.resolve().relative_to(config.ESGPULL_DATA_DIR)
+            )
+            Path.mkdir(regridded_dir, exist_ok=True)
+
+            for nc_file in tqdm(nc_files, desc=f"Regridding {raw_data_dir_fp}..."):
+                # get new fp
+                regridded_fp = regridded_dir / nc_file.name
+                # if file already exists, skip
+                if regridded_fp.exists():
+                    print(f"{regridded_fp} already exists.")
+                    continue
+
+                # Perform regridding operation here
+                print(f"\tRegridding {nc_file.stem}...", flush=True)
+                ds = processing.load_dataset_with_dask(
+                    nc_file, chunk_schema=chunk_schema
                 )
-                # make new directory for regridded data
-                regridded_dir = (
-                    regridded_data_dir_fp
-                    / raw_data_dir_fp.resolve().relative_to(config.ESGPULL_DATA_DIR)
+                regridded_ds = utils.process_xa_d(
+                    utils.cdo_remap(
+                        ds,
+                        remap_template_fp=remap_template_fp,
+                        remap_method=REMAP_METHOD,
+                    )
                 )
-                Path.mkdir(regridded_dir, exist_ok=True)
+                regridded_ds.to_netcdf(regridded_fp)
+                ds.close()
+                regridded_ds.close()
 
-                for nc_file in tqdm(nc_files, desc=f"Regridding {raw_data_dir_fp}..."):
-                    # get new fp
-                    regridded_fp = regridded_dir / nc_file.name
-                    # if file already exists, skip
-                    if regridded_fp.exists():
-                        print(f"{regridded_fp} already exists.")
-                        continue
-
-                    # Perform regridding operation here
-                    print(f"\tRegridding {nc_file.stem}...", flush=True)
-                    ds = processing.load_dataset_with_dask(
-                        nc_file, chunk_schema=chunk_schema
-                    )
-                    regridded_ds = utils.process_xa_d(
-                        utils.cdo_remap(
-                            ds,
-                            remap_template_fp=remap_template_fp,
-                            remap_method=REMAP_METHOD,
-                        )
-                    )
-                    regridded_ds.to_netcdf(regridded_fp)
-                    ds.close()
-                    regridded_ds.close()
-
-                if DO_DELETE_ORIGINAL:
-                    nc_file.unlink()
+            if DO_DELETE_ORIGINAL:
+                nc_file.unlink()
 
 
 def extract_levels(subdir):
