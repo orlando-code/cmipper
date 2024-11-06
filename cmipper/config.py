c6b29fc0034d6a9946e717e28bca3351409c1d29
Orlando Timmerman <rt582@cam.ac.uk>
2024-11-06 10:17:27 +0000

    nothing major

--- cmipper/config.py
+++ cmipper/config.py
@@ -12,13 +12,17 @@ def get_repo_dir():
 # Define global filepaths used throughout
 ################################################################################
 
-repo_dir = get_repo_dir()
-data_dir = repo_dir / "data"
-logging_dir = repo_dir / "logs"
-model_info = repo_dir / "model_info.yaml"
-download_config = repo_dir / "download_config.yaml"
+REPO_DIR = get_repo_dir()
+DATA_DIR = REPO_DIR / "data"
+ESGPULL_DIR = REPO_DIR / ".esgpull"
+ESGPULL_DATA_DIR = ESGPULL_DIR / "data"
+LOGGING_DIR = REPO_DIR / "logs"
+MODEL_INFO = REPO_DIR / "model_info.yaml"
+DOWNLOAD_CONFIG = REPO_DIR / "download_config.yaml"
+TEST_DATA_DIR = DATA_DIR / "test"
+TMP_DIR = REPO_DIR / "tmp"
 
-cmip6_data_dir = data_dir / "env_vars" / "cmip6"
+CMIP6_DATA_DIR = DATA_DIR / "env_vars" / "cmip6"
 
 # TODO: automate creation of example figures and videos from downloads. But who has the time?
 # figure_folder = "figures"
